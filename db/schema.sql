cdfa52ee63b769bfc4030fcbfe15bc16495103d7
DN-Vanguard <Domiltannguyen@outlook.com>
2021-08-02 23:20:51 -0700

    schema commands have been created. Now updating seeds.sql

---
+++ db/schema.sql
@@ -0,0 +1,29 @@
+-- implying that the user has not yet created or have an employees_db, therefore, no need for DROP DATABASE IF EXIST
+CREATE DATABASE employees_db;
+USE employees_db;
+CREATE TABLE department (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(30) NOT NULL
+);
+CREATE TABLE role (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(30) NOT NULL,
+    salary DECIMAL NOT NULL,
+    department_id INT,
+    FOREIGN KEY (department_id)
+        REFERENCES department(id)
+        ON DELETE SET NULL
+);
+CREATE TABLE employee (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    role_id INT,
+    manager_id INT DEFAULT null,
+    FOREIGN KEY (role_id)
+        REFERENCES role(id)
+        ON DELETE CASCADE,
+    FOREIGN KEY (manager_id)
+        REFERENCES employee(id)
+        ON DELETE SET NULL
+);
