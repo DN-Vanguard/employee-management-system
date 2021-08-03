dc3e2d1d29f27c162a22dda48e3c4ed4f02993fb
DN-Vanguard <Domiltannguyen@outlook.com>
2021-08-02 23:33:12 -0700

    added in viewing function of dept table

--- index.js
+++ index.js
@@ -3,5 +3,39 @@ require('dotenv').config();
 const inquirer = require('inquirer');
 const mysql = require('mysql2');
 const util = require('util');
-const questions = require('./assets/questions');
-const queries = require('./assets/queries');
+const questions = require('./lib/questions');
+const queries = require('./lib/queries');
+
+// Connect to the employees_db
+const db = mysql.createConnection(
+    {
+      host: 'localhost',
+      user: 'root',
+      password: process.env.DB_PASS,
+      database: 'employees_db'
+    },
+);
+
+// Turn database queries into a promise
+db.query = util.promisify(db.query);
+
+// Display the table to the console
+const viewTable = (table) => {
+    console.log('\n');
+    console.table(table);
+    console.log('\n');
+}
+
+// View the data on the 'department' table
+const selectDepartmentTable = async () => {
+    try {
+        const table = await db.query(queries.departments);
+    
+        viewTable(table);
+
+        return askForCategory();
+
+    } catch (err) {
+        console.log(err);
+    }
+}
